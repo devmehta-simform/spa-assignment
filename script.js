56343cc2d8207c3ef4f87efdea52d433c3b07802
dev mehta <dev.mehta@simformsolutions.com>
2025-01-21 13:37:34 +0530

    added navigation using js and styled the current link

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 document.addEventListener("scroll", (e) => {
   const height = window.scrollY;
-  if (height >= 20) {
-    document.querySelector("header").style.marginTop = "-10.5rem";
-  }
-  if (height < 20) {
+  if (height >= 15) {
+    document.querySelector("header").style.marginTop = "-30vh";
+  } else if (height < 30) {
     document.querySelector("header").style.marginTop = "0";
   }
-  console.log(height);
 });
+const section = document.querySelector("section");
+document.querySelectorAll("a").forEach((aEle) => {
+  aEle.addEventListener("click", (e) => {
+    e.preventDefault();
+    route(e);
+  });
+});
+function route(e) {
+  const currlink = document.querySelector(".current_link");
+  const newlink = document.querySelector("#" + e.target.id);
+  currlink.classList = "";
+  newlink.classList = "current_link";
+  const currele = section.querySelector(".current_page");
+  const newele = section.querySelector("#" + e.target.id.split("_link")[0]);
+  currele.className = "";
+  newele.className = "current_page";
+}
