a34a4541c9d015c543e3c9e66d3c777d4c0abbd0
dev mehta <dev.mehta@simformsolutions.com>
2025-01-30 13:03:13 +0530

    pagination custom input added and handled

--- scripts/PaginationUtil.mjs
+++ scripts/PaginationUtil.mjs
@@ -1,8 +1,16 @@
-const pagOffset = 4;
+let pagOffset = 1;
 let currbtn = 1;
 let className;
-export function HandlePagination(classNameArg) {
+document.querySelector("#pagination-input")?.addEventListener("change", (e) => {
+  pagOffset = parseInt(e.target.value);
+  document.querySelector(`.${className}-pagination-btns-container`).innerHTML =
+    "";
+  HandlePagination(className, pagOffset);
+});
+export function HandlePagination(classNameArg, pagOffset = 1) {
   className = classNameArg;
+  // console.log("HandlePagination", pagOffset);
+
   if (
     !document
       .querySelector(`.${className}-pagination-btns-container`)
@@ -16,27 +24,28 @@ export function HandlePagination(classNameArg) {
       pagOffset
     );
   }
-  handlePagination(currbtn);
+  handlePagination(currbtn, pagOffset);
 }
 
-function initPagBtns(parentEle, nitems, offset) {
-  const nbtns = Math.ceil(parseFloat(nitems) / parseFloat(offset));
-  //   console.log(nbtns);
+function initPagBtns(parentEle, nitems, pagOffset) {
+  const nbtns = Math.ceil(parseFloat(nitems) / parseFloat(pagOffset));
+  // console.log("initPagBtns nbtns", nbtns);
   for (let i = 1; i <= nbtns; i++) {
     const btn = document.createElement("button");
     btn.id = `btn-${i}`;
     btn.classList.add("pagination-btn");
     btn.textContent = i;
     btn.addEventListener("click", (e) => {
-      handlePagination(e.target.id.split("-")[1]);
+      handlePagination(e.target.id.split("-")[1], pagOffset);
     });
     parentEle.appendChild(btn);
   }
   //   <button class="pagination-btn" id="btn-1">1</button><button class="pagination-btn" id="btn-2">2</button><button class="pagination-btn" id="btn-3">3</button><button class="pagination-btn" id="btn-4">4</button><button class="pagination-btn" id="btn-5">5</button>
 }
 
-function handlePagination(btnid) {
-  //   console.log(btnid);
+function handlePagination(btnid, pagOffset) {
+  // console.log("handlePagination btnid", btnid);
+  // console.log("handlePagination pageOffset", btnid);
 
   const items = document.querySelectorAll(
     `.${className}-items-container .${className}-item`
@@ -49,6 +58,16 @@ function handlePagination(btnid) {
   items.forEach((item, i) => {
     // const itemId = parseInt(item.id.split("-")[2]);
     const tmpval = btnid * pagOffset - i;
+    // console.log(
+    //   "btnid",
+    //   btnid,
+    //   "pageoffset",
+    //   pagOffset,
+    //   "i",
+    //   i,
+    //   "tmpval",
+    //   tmpval
+    // );
 
     if (tmpval > 0 && tmpval <= pagOffset) {
       // console.clear();
@@ -61,8 +80,8 @@ function handlePagination(btnid) {
         : item.classList.add("show");
       //   if (tmpval == 0) item.style.border = "none";
     } else {
-      console.clear();
-      console.log(i, tmpval);
+      // console.clear();
+      // console.log(i, tmpval);
       //   item.style.display = "none";
       item.classList.contains("show")
         ? item.classList.replace("show", "hide")
